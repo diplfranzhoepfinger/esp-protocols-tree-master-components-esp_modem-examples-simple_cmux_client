e19d957917b39d81fb3c96b311c31f2a14d7f383
Franz Höpfinger <krone-trailer@franzhoepfinger.de>
2023-05-19 11:50:10 +0200

    modem_pwkey

---
+++ simple_cmux_client/components/modem_pwkey/modem_pwkey.h
@@ -0,0 +1,18 @@
+/*
+ * modem_pwkey.h
+ *
+ *  Created on: 30.04.2022
+ *      Author: franz
+ */
+
+#ifndef COMPONENTS_MODEM_PWKEY_MODEM_PWKEY_H_
+#define COMPONENTS_MODEM_PWKEY_MODEM_PWKEY_H_
+
+
+void init_modem_pwkey(void);
+void power_up_modem_pwkey(void);
+void power_down_modem_pwkey(void);
+void power_reset_modem_pwkey(void);
+
+
+#endif /* COMPONENTS_MODEM_PWKEY_MODEM_PWKEY_H_ */
