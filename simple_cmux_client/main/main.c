5319c9a8a50d0f61c1911e430045e8384d6eeeae
Franz Höpfinger <krone-trailer@franzhoepfinger.de>
2023-05-31 17:19:02 +0200

    power down first now.

--- simple_cmux_client/main/main.c
+++ simple_cmux_client/main/main.c
@@ -6,17 +6,13 @@ extern void simple_cmux_client_main(void);
 void app_main(void)
 {
 
-
-
     init_modem_pwkey();
+
+    power_down_modem_pwkey();
 
     // power Up does work now. .
     power_up_modem_pwkey();
 
-
-
     simple_cmux_client_main();
-
-
 
 }
