f421612216b2eaa5ea284ab928b13d6c09df6c56
Franz Höpfinger <krone-trailer@franzhoepfinger.de>
2023-07-03 17:18:09 +0200

    start CMUX again.

--- simple_cmux_client/components/simple_cmux_client/simple_cmux_client_main.cpp
+++ simple_cmux_client/components/simple_cmux_client/simple_cmux_client_main.cpp
@@ -445,10 +445,6 @@ extern "C" void simple_cmux_client_main(void)
     vTaskDelay(10000 / portTICK_PERIOD_MS);
 
 
-    /* Again reading some data from the modem */
-    if (dce->get_imsi(str) == esp_modem::command_result::OK) {
-        std::cout << "Modem IMSI number:" << str << std::endl;
-    }
 
 
 
@@ -485,7 +481,46 @@ extern "C" void simple_cmux_client_main(void)
 
 
 
+    // wait 10s
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
 
 
+    /* Again reading some data from the modem */
+    if (dce->get_imsi(str) == esp_modem::command_result::OK) {
+        std::cout << "Modem IMSI number:" << str << std::endl;
+    }
+
+    // wait 10s
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
+
+#ifdef SUPPORT_URC_HANDLER
+    ESP_LOGI(TAG, "Removing URC handler");
+    dce->set_on_read(nullptr);
+#endif
+    if (dce->set_mode(esp_modem::modem_mode::CMUX_MANUAL_MODE)) {
+        std::cout << "Modem has correctly entered CMUX_MANUAL_MODE" << std::endl;
+    } else {
+        ESP_LOGE(TAG, "Failed to configure CMUX_MANUAL_MODE... exiting");
+        return;
+    }
+    if (dce->set_mode(esp_modem::modem_mode::CMUX_MANUAL_DATA)) {
+        std::cout << "Modem has correctly entered CMUX_MANUAL_DATA" << std::endl;
+    } else {
+        ESP_LOGE(TAG, "Failed to configure CMUX_MANUAL_DATA... exiting");
+        return;
+    }
+#ifdef SUPPORT_URC_HANDLER
+    ESP_LOGI(TAG, "Adding URC handler");
+    dce->set_on_read(handle_urc);
+#endif
+
+
+    // wait 10s
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
+
+    /* Again reading some data from the modem */
+    if (dce->get_imsi(str) == esp_modem::command_result::OK) {
+        std::cout << "Modem IMSI number:" << str << std::endl;
+    }
 
 }
